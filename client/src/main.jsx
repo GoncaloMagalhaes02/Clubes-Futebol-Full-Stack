3c830df5742f348a8b4b561d5b61927fd7aa4f7f
GoncaloMagalhaes02 <goncamag02@gmail.com>
2025-12-16 11:23:30 +0000

    initial setup react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, React } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import RootLayout from "./RootLayout.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
